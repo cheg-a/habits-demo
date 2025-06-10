d52b633d502434290c0bc6a8e0aabe3e3f8eeb02
Anton Nikolenko <anton.nikolenko@ys.bet>
2025-06-10 12:33:17 +0300

    temp

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/habits-demo/',
-  server: {
-    allowedHosts: ['habbit.local'], // Добавляем разрешенные хосты
-    proxy: {
-      '/api': {
-        target: 'http://api.habbit.local:3000', // URL вашего бэкенда
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Удаляем /api из пути
-      },
-    },
-  },
 })
